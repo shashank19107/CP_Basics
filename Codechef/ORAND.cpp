e6ba6114db296df914dffcca22e6ed216b56290e
Shashank Dargar <55681562+shashank19107@users.noreply.github.com>
2021-01-14 23:01:49 +0530

    Create ORAND.cpp

---
+++ Codechef/ORAND.cpp
@@ -0,0 +1,15 @@
+//I wasn't able to solve this problem.
+
+//Statement: https://discuss.codechef.com/t/orand-editorial/83399
+
+*/
+References:
+
+    https://csacademy.com/blog/fast-fourier-transform-and-variations-of-it
+    
+A similar problem: https://csacademy.com/contest/archive/task/and-closure/solution/
+          >> Check Editorial.
+          >> eg. code: https://csacademy.com/submission/86719/
+
+
+/*
